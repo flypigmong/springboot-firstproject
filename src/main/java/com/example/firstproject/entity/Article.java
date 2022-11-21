d6c255f5b69b2a4ee9a05ba8aa58441c0f10c8d5
flypigmong <jseonghun85@gmail.com>
2022-11-21 22:26:42 +0900

    데이터 조회하기 with JPA No default constructor for entity : 디폴트생성자 만들어주기

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity //DB가 해당 객체를 인식 가능!
 @AllArgsConstructor
+@NoArgsConstructor //디폴트 생성자를 추가
 @ToString
 public class Article {
 
@@ -22,6 +24,9 @@ public class Article {
 
     @Column
     private String contents;
+
+/*    Article() {  //@NoArgsConstructor 로 대체!
+    }*/
 
 //리팩터링 ->@AllArgsConstructor
  /*   public Article(Long id, String title, String contents) {
