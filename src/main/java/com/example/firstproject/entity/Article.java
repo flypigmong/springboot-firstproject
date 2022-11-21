810e2775ee5741040d01d51284a17d9686bbfd5f
flypigmong <jseonghun85@gmail.com>
2022-11-21 21:02:32 +0900

    롬복과 리팩터링.. Article에서 @AllArgsConstructor , @ToString 으로 리팩터링

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity //DB가 해당 객체를 인식 가능!
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id  //대표값을 지정! Like a 주민등록번호
@@ -18,19 +23,20 @@ public class Article {
     @Column
     private String contents;
 
-
-    public Article(Long id, String title, String contents) {
+//리팩터링 ->@AllArgsConstructor
+ /*   public Article(Long id, String title, String contents) {
         this.id = id;
         this.title = title;
         this.contents = contents;
-    }
+    }*/
 
-    @Override
+    //@Tostring으로 리팩터링
+/*    @Override
     public String toString() {
         return "Article{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", contents='" + contents + '\'' +
                 '}';
-    }
+    }*/
 }
